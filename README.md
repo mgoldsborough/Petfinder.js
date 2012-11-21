5c0326b9337d636b5e9698cc333d02cf4cc57831
Mathew Goldsborough <mgoldsborough@mg2innovations.com>
2012-11-20 20:01:25 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,33 @@
+Petfinder.js
+------------
+Javascript library to interact with the Petfinder API through a proxy.
+
+Currently we use our own jsonp proxy to provide cross domain requests.  This will be open sourced eventually...
+
+Read the docs here: http://www.petfinder.com/developers/api-docs
+
+Get an API key here: http://www.petfinder.com/developers/api-key
+
+
+Requirements
+------------
+ * jQuery (http://jquery.com/)
+ * jQuery MD5 (http://archive.plugins.jquery.com/project/md5)
+
+
+License
+-------
+
+	Copyright 2012 MG2 Innovations (http://mg2innovations.com)
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	     http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
---
+++ petfinder.js
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2012 MG2 Innovations LLC (http://www.mg2innovations.com)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 	
+ *	http://www.apache.org/licenses/LICENSE-2.0 	
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.  
+ */
+
+var petfinder = new function() {
+	// TODO: Update for production proxy domain name.
+	var proxyUrl = 'http://localhost:8000/';
+ 	
+	var apiKey = '<YOUR_API_KEY>';
+	var apiSecret = '<YOUR_API_SECRET>';
+	
+	var authKey;
+	
+	this.getPet = function(callback, petId) {
+		var queryParams = {
+			'key': apiKey,
+			'id': petId,
+			'format': 'json',
+		}
+		
+		hit(callback, 'pet.get', queryParams);
+	}
+	
+	this.findPet = function(callback, location, animal, breed, size, sex, age, offset, count, output) {
+		var queryParams = {
+			'key': apiKey,
+			'format': 'json'
+		};
+		
+		if(location)
+			queryParams['location'] = location;
+		
+		if(animal)
+			queryParams['animal'] = animal;
+	
+		if(breed)
+			queryParams['breed'] = breed;
+		
+		if(size)
+			queryParams['size'] = size;
+		
+		if(sex)
+			queryParams['sex'] = sex;
+		
+		if(age)
+			queryParams['age'] = age;
+			
+		if(offset)
+			queryParams['offset'] = offset;
+		
+		if(count)
+			queryParams['count'] = count;
+		
+		if(output)
+			queryParams['output'] = output;
+		
+		hit(callback, 'pet.find', queryParams);
+	}
+	
+	this.authenticate = function(callback) {
+		var queryParams = {
+			'key': apiKey,
+			'format': 'json',
+		}
+		
+		hit(callback, 'auth.getToken', queryParams);
+	};
+	
+	var hit = function(callback, method, queryParams) {
+		var queryString = genQueryString(queryParams);
+		console.debug('Query String: ' + queryString);
+		
+		var url = proxyUrl + method + '?' + queryString;
+		console.debug('URL: ' + url);
+		
+		$.getJSON(
+			url + '&callback=?',
+			function(data, textStatus, jqXHR) {
+				callback(data);
+			}
+		);
+	};
+	
+	var getKeys = function(obj) {
+		// Get keys from array
+		var keys = [];
+		for(var key in obj){
+			if(obj.hasOwnProperty(key)) {
+				keys.push(key);
+			}
+		}
+		return keys;
+	}
+	
+	var genQueryString = function(queryParams) {
+		var queryString = '';
+		
+		// Get keys from parameters and sort
+		var keys = getKeys(queryParams);
+		
+		// Sort keys and iterate to get name/value pairs.
+		// Build value to be hashed.
+		for (var key in keys.sort()) {
+			var name = keys[key];
+			var value = queryParams[name];
+			queryString += "&" + name + "=" + value
+		}
+		
+		// Generate and append sig
+		queryString += "&sig=" + $.md5(apiSecret + queryString);
+		
+		return queryString;
+	};
+};
